07bf20a0d8bb4f72ae382bb6d65d79b4f0ed8219
M S Akter <shahinurmony4@gmail.com>
2023-03-04 02:18:59 +0600

    modal opened

--- js/index.js
+++ js/index.js
@@ -34,14 +34,14 @@ const displayItems = (items, dataLimit) => {
                               <h5 class="card-title">${item.name}</h5>
                               <div class="d-flex justify-content-around mt-4">
                               <div class="me-auto"><i class="fa-solid fa-calendar-days"></i>   ${item.published_in}</div>
-                              <div><i class="text-danger fa-solid fa-arrow-right"></i></div>
+                              <div onclick="loadItemDetails()" data-bs-toggle="modal" data-bs-target="#itemDetailModal"><i class="text-danger fa-solid fa-arrow-right"></i></div>
                               </div>
                             </div>
                         </div>
                     `
         itemsContainer.appendChild(itemDiv);
     })
-    toggleSpinner(false)
+    toggleSpinner(false);
 }
 
 const searchProcess = (dataLimit) => {
@@ -63,7 +63,32 @@ document.getElementById('btn-see-more').addEventListener('click', function () {
     searchProcess();
 })
 
-
+const loadItemDetails = (id) =>{
+        const url =` https://openapi.programming-hero.com/api/ai/tool/01`;
+        console.log(url)
+        // fetch(url);
+        // .then(res=> res.json());
+        // .then(data=>console.log(data))
+    }
+    // const loadMeals = (searchText) => {
+        //     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+        //     console.log(url)
+        //     fetch(url)
+        //       .then(res => res.json())
+        //       .then(data => displayMeals(data.meals))
+        //   }
+    loadItemDetails()
+    // const displayItemDetails = item =>{
+    //     console.log(item);
+    //     const modalTitle = document.getElementById('itemDetailModalLabel');
+    //     modalTitle.innerText = item.name;
+    //     const itemDetails = document.getElementById('item-details');
+    //     itemDetails.innerHTML=`
+    //     <p>Release Date: ${item.releaseDate ? item.releaseDate : 'No Release Date Found'}</p>
+    //     <p>Storage: ${item.mainitems.storage ? item.mainitems.storage :'No Storage Information'}</p>
+    //     <p>Others: ${item.others? item.others.Bluetooth : 'No Bluetooth Information'}</p>
+    //     `
+    // }
 
 loadItems(6)
 
@@ -118,7 +143,7 @@ loadItems(6)
 //                 <div class="card-body">
 //                   <h5 class="card-title">${item.item_name}</h5>
 //                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//                   <button onclick="loaditemDetails('${item.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#itemDetailModal">Show Details</button>
+//                   <button onclick="loadItemDetails('${item.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#itemDetailModal">Show Details</button>
 //                 </div>
 //             </div>
 //         `
@@ -179,3 +204,72 @@ loadItems(6)
 //         return liHTML
 //     }
 // }
+
+
+
+// const loadMeals = (searchText) => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+//     console.log(url)
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(data => displayMeals(data.meals))
+//   }
+//   const displayMeals = meals => {
+//     // console.log(meals)
+//     const mealsContainer = document.getElementById('meals-container')
+//     mealsContainer.innerText = '';
+//     meals.forEach(meal => {
+//       console.log(meal)
+//       const mealDiv = document.createElement('div')
+//       mealDiv.classList.add('col')
+//       mealDiv.innerHTML = `
+//           <div class="col">
+//                 <div class="card">
+//               <img src="${meal.strMealThumb}
+//               " class="card-img-top" alt="...">
+//                   <div class="card-body">
+//                     <h5 class="card-title">${meal.strMeal}</h5>
+//                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//                   <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+//                       Details
+//                         </button>
+//                   </div>
+//                 </div>
+//           </div>
+//           `
+//       mealsContainer.appendChild(mealDiv)
+//     })
+//   }
+//   const searchMeals = () => {
+//     const searchText = document.getElementById('search-field').value;
+//     console.log(searchText)
+//     loadMeals(searchText)
+//   }
+//   const loadMealDetail = idMeal=>{
+//     const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+//     fetch(url)
+//     .then(res=>res.json())
+//     .then(data=>displayMealDetails(data.meals[0]))
+//     .catch(error=>{console.log(error)})
+//   }
+//   // const loadMealDetail2= async(idMeal){
+//   //   const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+//   //   try{
+//     // const res = await fetch(url);
+//   //   const data = await res.json();
+//   //   displayMealDetails(data.meals[0])
+//   // }
+//   // catch(error){
+//   //   console.log(error)
+//   // }
+//   // }
+  
+//   const displayMealDetails= meal =>{
+//     console.log(meal)
+//     document.getElementById('mealDetailsLabel').innerText= meal.strMeal;
+//     const mealsDetails =document.getElementById('mealDetailBody');
+//     mealsDetails.innerHTML= `
+//     <img class="img-fluid" src="${meal.strMealThumb}">
+//     `
+//   }
+//   loadMeals('fish')
